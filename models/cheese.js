a91b5378be74b4f02a622785063cf433b7c67080
Arash <arashsaleh798@gmail.com>
2025-02-14 16:17:08 -0800

    created new cheese page

---
+++ models/cheese.js
@@ -0,0 +1,12 @@
+// models/cheese.js
+
+const mongoose = require("mongoose");
+
+const cheeseSchema = new mongoose.Schema ({
+    name: String, 
+    isStinky: Boolean,
+});
+
+const Cheese = mongoose.model("Cheese", cheeseSchema);
+
+module.exports = Cheese;
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-const express = require('express');
+const dotenv = require("dotenv"); 
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+
+const Cheese = require("./models/cheese");
+
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+  
+app.get("/cheeses/new", (req, res)=> {
+    res.render("cheeses/new.ejs");
 });
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
