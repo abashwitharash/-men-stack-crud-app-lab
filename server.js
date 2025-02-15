485ec9651c3df7ec87af46f441d3a89a03876440
Arash <arashsaleh798@gmail.com>
2025-02-15 15:47:06 -0800

    lab complete and added styling

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const path = require("path");
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -20,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method")); 
 // app.use(morgan("dev")); 
 
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET /
 app.get("/", async (req, res) => {
